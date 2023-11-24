c8585ec19ebcf7545a58ec86af1e42cc8700888d
awe <aweful@awews.com>
2023-11-23 18:57:04 -0700

    log response to movies end point

--- pages/api/favorites.ts
+++ pages/api/favorites.ts
@@ -19,6 +19,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
     });
 
+    console.log('favoritedMovies :>> ', favoritedMovies);
+
     return res.status(200).json(favoritedMovies);
   } catch (error) {
     console.log(error);
