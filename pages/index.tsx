250d2b4f76d0efa18dc4b5e6bb703dba45fda3ee
awe <aweful@awews.com>
2023-11-23 18:57:24 -0700

    comment out favorites component

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,7 +70,7 @@ const Home = () => {
       <Billboard />
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies} />
-        <MovieList title="My List" data={favorites} />
+        {/* <MovieList title="My List" data={favorites} /> */}
       </div>
     </>
   )
