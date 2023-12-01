71cbdfe6a8dfe60ca638824191c2b4b3bafacc99
dapperAuteur <a@awews.com>
2023-12-01 11:40:50 -0700

    refactor to use graphql api, not returning obj, needs more work

--- hooks/useMovie.ts
+++ hooks/useMovie.ts
@@ -1,12 +1,23 @@
 import useSwr from 'swr'
-import fetcher from '@/libs/fetcher';
+import { fetcher } from '@/libs/fetcher';
 
-const useMovie = (id?: string) => {
-  const { data, error, isLoading } = useSwr(id ? `/api/movies/${id}` : null, fetcher, {
+const useMovie = (_id?: string) => {
+  // console.log('useMovie _id :>> ', _id);
+  const { data, error, isLoading } = useSwr(_id ? `findMediaByID(
+    _id: ${_id}){
+      _id
+      title
+      media_link
+      thumbnail_url
+      description
+  }` : null, fetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
   });
+  let media = data?.findMediaByID;
+  // console.log('useMovie media :>> ', media);
+
   return {
     data,
     error,
