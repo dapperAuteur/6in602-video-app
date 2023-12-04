5339a71328ba33b63d3ba5c70a42b919f7210fba
dapperAuteur <a@awews.com>
2023-12-03 21:26:37 -0700

    add comment to communicate when to use each url

--- libs/fetcher.ts
+++ libs/fetcher.ts
@@ -1,7 +1,7 @@
 export function fetcher(query: any) {
   // console.log("fetcher query", query);
-  return fetch('https://code-word-list.witus.online/api/graphql', {
-  // return fetch('http://localhost:3000/api/graphql', {
+  return fetch('https://code-word-list.witus.online/api/graphql', { // production api
+  // return fetch('http://localhost:3000/api/graphql', { // localhost api
     method: 'POST',
     headers: {
       'Content-type': 'application/json',
